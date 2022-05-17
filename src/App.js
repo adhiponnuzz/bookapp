c43a4a17cfbab451d15d7cfefe31c48919cd569c
Adhiponnuzz <adithyaminnu689693@gmail.com>
2022-05-17 13:02:09 +0530

    Searchbook finished

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Addbooks from './component/Addbooks';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Searchbook from './component/Searchbook';
 function App() {
   return (
     
     <>
     <Addbooks/>
+    <Searchbook/>
     </>
 
 
--- src/component/Searchbook.js
+++ src/component/Searchbook.js
@@ -1,10 +1,16 @@
+import React from 'react'
+
+const Searchbook = () => {
+  return (
+    <div>
+
 <div className="container">
     <div className="row">
         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <label for="" className="form-label">Book Title</label>
-                    <input placeholder="Enter Book Title" type="text" className="form-control">
+                    <input placeholder="Enter Book Title" type="text" className="form-control"/>
                 </div>
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <button className="btn btn-primary">SEARCH</button>
@@ -13,3 +19,11 @@
         </div>
     </div>
 </div>
+
+
+    </div>
+
+  )
+}
+
+export default Searchbook
