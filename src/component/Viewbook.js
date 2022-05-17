b73dd08f812c3a9dd21bb0a112a509a2b607bbd0
Adhiponnuzz <adithyaminnu689693@gmail.com>
2022-05-17 15:14:34 +0530

    viewbook finish

--- src/component/Viewbook.js
+++ src/component/Viewbook.js
@@ -12,12 +12,12 @@ const Viewbook = () => {
 
        <Navbar/>
 
-       <div class="container">
-    <div class="row">
-        <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+       <div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
            {booklist.title}
         </div>
-        <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
             {booklist.price}
          </div>
          
